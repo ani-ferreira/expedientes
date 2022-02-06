be7072d09f7c59ebc68595ead1c6c7959062aa17
anaferreira <estudioj.ferreira@gmail.com>
2022-02-06 04:11:20 -0300

    Changed login form style

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { loginUser } from '../Services/authServices';
+import { Card } from 'react-bootstrap';
 
 const Login = () => {
   const [data, setdata] = useState({
@@ -19,43 +20,45 @@ const Login = () => {
   };
 
   return (
-    <form onSubmit={(e) => submitHandler(e)}>
-      <div className="mb-3">
-        <label htmlFor="exampleInputEmail1" className="form-label">
-          Email
-        </label>
-        <input
-          type="email"
-          className="form-control"
-          id="exampleInputEmail1"
-          aria-describedby="emailHelp"
-          onChange={(e) => {
-            inputHandler(e);
-          }}
-          value={data.email}
-          name="email"
-        />
-        <div id="emailHelp" className="form-text">
-          We'll never share your email with anyone else.
+    <Card className="w-50 p-5 container">
+      <form onSubmit={(e) => submitHandler(e)}>
+        <div className="mb-3">
+          <label htmlFor="exampleInputEmail1" className="form-label">
+            Email
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+            onChange={(e) => {
+              inputHandler(e);
+            }}
+            value={data.email}
+            name="email"
+          />
+          <div id="emailHelp" className="form-text">
+            We'll never share your email with anyone else.
+          </div>
         </div>
-      </div>
-      <div className="mb-3">
-        <label htmlFor="exampleInputPassword1" className="form-label">
-          Contraseña
-        </label>
-        <input
-          type="password"
-          className="form-control"
-          id="exampleInputPassword1"
-          onChange={(e) => {
-            inputHandler(e);
-          }}
-          name="password"
-          value={data.password}
-        />
-      </div>
-      <button className="btn btn-primary">Ingresar</button>
-    </form>
+        <div className="mb-3">
+          <label htmlFor="exampleInputPassword1" className="form-label">
+            Contraseña
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            id="exampleInputPassword1"
+            onChange={(e) => {
+              inputHandler(e);
+            }}
+            name="password"
+            value={data.password}
+          />
+        </div>
+        <button className="btn btn-secondary">Ingresar</button>
+      </form>
+    </Card>
   );
 };
 
