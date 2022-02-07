16ad5824618d6b1c255d1dd72ead597b913e4b3a
anaferreira <estudioj.ferreira@gmail.com>
2022-02-07 19:56:10 -0300

    Split homecards from home component

---
+++ client/src/components/HomeCards.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from '../components/styles/Home.module.css';
+import { Container, Row, Col, Card } from 'react-bootstrap';
+import {
+  BsFillCloudArrowUpFill,
+  BsFillArchiveFill,
+  BsCardList,
+} from 'react-icons/bs';
+
+const HomeCards = () => {
+  return (
+    <Container className={styles.container}>
+      <Row className="gy-5">
+        <Col>
+          <Card className={styles.card}>
+            <div>
+              <BsFillCloudArrowUpFill className={styles.icon} />
+            </div>
+            <Card.Body className={styles.text}>
+              <Card.Title>
+                <Link to="/add" className={styles.link}>
+                  Agregar movimiento
+                </Link>
+              </Card.Title>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col>
+          <Card className={styles.card}>
+            <div>
+              <BsCardList className={styles.icon} />
+            </div>
+            <Card.Body className={styles.text}>
+              <Card.Title>
+                <Link to="/posts" className={styles.link}>
+                  Ver todos los movimientos
+                </Link>
+              </Card.Title>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col>
+          <Card className={styles.card}>
+            <div>
+              <BsFillArchiveFill className={styles.icon} />
+            </div>
+            <Card.Body className={styles.text}>
+              <Card.Title>
+                <Link to="/folders" className={styles.link}>
+                  Carpetas
+                </Link>
+              </Card.Title>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default HomeCards;
