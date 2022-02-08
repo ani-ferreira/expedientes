cd862ad0aadd475289354a6b95b3dbf72eeb5acd
anaferreira <estudioj.ferreira@gmail.com>
2022-02-08 19:57:23 -0300

    removed login from home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,13 +1,7 @@
 import Links from '../components/Links';
-import { useSelector } from 'react-redux';
 import HomeCards from '../components/HomeCards';
 
 const Home = () => {
-  const isLogged = useSelector((state) => state.authReducer.isAuth);
-  const token = useSelector((state) => state.authReducer.token);
-  console.log(isLogged);
-  console.log(token);
-
   return (
     <>
       <HomeCards />
