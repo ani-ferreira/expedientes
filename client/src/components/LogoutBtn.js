23732a3492a48d1ef2641ce32571cff386e3e978
anaferreira <estudioj.ferreira@gmail.com>
2022-02-08 21:26:37 -0300

    added redirect to login on logout btn

--- client/src/components/LogoutBtn.js
+++ client/src/components/LogoutBtn.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
 const LogoutBtn = () => {
+  const history = useHistory();
+
+  const logout = () => {
+    localStorage.removeItem('token');
+    history.replace('/login');
+  };
+
   return (
     <button
       className="btn btn-danger"
       onClick={() => {
-        localStorage.removeItem('token');
+        logout();
       }}
     >
       Logout
