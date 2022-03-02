e51c27a01209f17055d76a53325c19e0ced9ec8a
anaferreira <estudioj.ferreira@gmail.com>
2022-03-01 22:13:40 -0300

    Changed btn text

--- client/src/pages/NotFound.js
+++ client/src/pages/NotFound.js
@@ -1,12 +1,12 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 const NotFound = () => {
   return (
     <div className="container title margin">
       <h1>Esta página no existe</h1>
-      <h1>Visitá la página principal:</h1>
+
       <button className="btn btn-secondary btn-lg margin" type="button">
         <Link to="/" className="text-decoration-none text-white">
-          INICIO
+          Volver al inicio
         </Link>
       </button>
     </div>
