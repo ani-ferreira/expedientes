86b21264738607674f65b13f439bfb422945c21b
anaferreira <estudioj.ferreira@gmail.com>
2022-03-12 20:25:05 -0300

    heroku configs

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const res = require('express/lib/response');
 require('dotenv/config');
+const path = require('path');
 
 //Connect to DB
-mongoose.connect(process.env.DB_CONNECTION);
+mongoose.connect(process.env.MONGODB_URI);
 
 const app = express();
 
@@ -19,17 +21,15 @@ app.use('/posts', require('./routes/post'));
 //Auth
 app.use('/user', require('./routes/auth'));
 
-// Accessing the path module
-const path = require('path');
+//production
 
-// Step 1: import the client build folder to the server
-app.use(express.static(path.resolve(__dirname, './client/build')));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
 
-// Step 2: ensure that the routes defined with React Router are working once the application has been deployed
-// It handles any requests by redirecting them to index.html
-app.get('*', function (request, response) {
-  response.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
-});
+  app.get('*', () => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
