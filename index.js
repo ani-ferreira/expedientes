219a63b2433a45d5ef281517aa889cec6278ccde
anaferreira <estudioj.ferreira@gmail.com>
2022-03-12 19:59:47 -0300

    heroku configs

--- index.js
+++ index.js
@@ -19,5 +19,17 @@ app.use('/posts', require('./routes/post'));
 //Auth
 app.use('/user', require('./routes/auth'));
 
+// Accessing the path module
+const path = require('path');
+
+// Step 1: import the client build folder to the server
+app.use(express.static(path.resolve(__dirname, './client/build')));
+
+// Step 2: ensure that the routes defined with React Router are working once the application has been deployed
+// It handles any requests by redirecting them to index.html
+app.get('*', function (request, response) {
+  response.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
