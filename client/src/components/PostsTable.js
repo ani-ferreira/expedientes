277b7566033016cf19e101276f2c1214fe53564c
anaferreira <estudioj.ferreira@gmail.com>
2022-03-12 22:38:50 -0300

    Changed table styles

--- client/src/components/PostsTable.js
+++ client/src/components/PostsTable.js
@@ -12,60 +12,58 @@ const PostsTable = (props) => {
   });
 
   return (
-    <div class="table-responsive">
-      <table className="table">
-        <thead>
+    <table className="table">
+      <thead>
+        <tr>
+          <th scope="col">ID</th>
+          <th scope="col">Fecha</th>
+          <th scope="col">Expediente</th>
+          <th scope="col">Movimiento</th>
+          <th scope="col">Tipo</th>
+          <th scope="col">Acciones</th>
+        </tr>
+      </thead>
+      {posts ? (
+        <tbody>
+          {filteredPosts
+            .map((post, index) => (
+              <tr key={index}>
+                <th scope="row">{index}</th>
+                <td>{post.fecha}</td>
+                <td>{post.expediente}</td>
+                <td>{post.movimiento}</td>
+                <td>{post.tipo}</td>
+                <td>
+                  <Link
+                    to={`/edit/${post._id}`}
+                    className="btn btn-outline-warning"
+                  >
+                    Editar
+                  </Link>
+                  &nbsp;
+                  <button
+                    className="btn btn-outline-danger"
+                    onClick={() => {
+                      props.onDelete(post._id);
+                    }}
+                  >
+                    Eliminar
+                  </button>
+                </td>
+              </tr>
+            ))
+            .reverse()}
+        </tbody>
+      ) : (
+        <tbody>
           <tr>
-            <th scope="col">ID</th>
-            <th scope="col">Fecha</th>
-            <th scope="col">Expediente</th>
-            <th scope="col">Movimiento</th>
-            <th scope="col">Tipo</th>
-            <th scope="col">Acciones</th>
+            <td>
+              <LoadingSpinner />
+            </td>
           </tr>
-        </thead>
-        {posts ? (
-          <tbody>
-            {filteredPosts
-              .map((post, index) => (
-                <tr key={index}>
-                  <th scope="row">{index}</th>
-                  <td>{post.fecha}</td>
-                  <td>{post.expediente}</td>
-                  <td>{post.movimiento}</td>
-                  <td>{post.tipo}</td>
-                  <td>
-                    <Link
-                      to={`/edit/${post._id}`}
-                      className="btn btn-outline-warning"
-                    >
-                      Editar
-                    </Link>
-                    &nbsp;
-                    <button
-                      className="btn btn-outline-danger"
-                      onClick={() => {
-                        props.onDelete(post._id);
-                      }}
-                    >
-                      Eliminar
-                    </button>
-                  </td>
-                </tr>
-              ))
-              .reverse()}
-          </tbody>
-        ) : (
-          <tbody>
-            <tr>
-              <td>
-                <LoadingSpinner />
-              </td>
-            </tr>
-          </tbody>
-        )}
-      </table>
-    </div>
+        </tbody>
+      )}
+    </table>
   );
 };
 
