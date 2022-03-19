a8b197f23d3a5d4b5397bfa803a63a7c9352eb18
anaferreira <estudioj.ferreira@gmail.com>
2022-03-19 16:10:10 -0300

    Get token from localstorage

--- middlewares/verifytoken.js
+++ middlewares/verifytoken.js
@@ -6,10 +6,13 @@ require('dotenv').config();
 
 module.exports = function (req, res, next) {
   const authHeader = req.header('authorization');
-  if (!authHeader?.startsWith('Bearer '))
+  if (
+    !authHeader?.startsWith('Bearer ') &&
+    localStorage.getItem('token') === ' '
+  )
     return res.status(401).send('access denied');
 
-  const token = authHeader.split(' ')[1];
+  const token = authHeader.split(' ')[1] || localStorage.getItem('token');
 
   jwt.verify(token, process.env.TOKEN_SECRET, (err, decoded) => {
     if (err) return res.sendStatus(403); //invalid token
